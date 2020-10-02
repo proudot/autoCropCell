0bb4b5213db04127df41617b49eb9f58e2904463
proudot <philippe.roudot@gmail.com>
2020-10-01 22:02:58 -0500

    add comment

--- autoCropCell.sh
+++ autoCropCell.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/sh
-gmic $1 -a z  --blur 5 -gt[-1] 5% -label[-1] 0  -split_colors[-1] 0 -rm[1] -dilate[1--1] 40 -repeat \{\$\!-1\}  --autocrop_seq[0,1] 0 -rm[1] -rm[-1]  -done -rm[0] -o $2
+gmic $1 -a z \
+     -e \"::: threshold and segment 5% of blurred data\" \
+     --blur 5 -gt[-1] 5% -label[-1] 0  \
+     -e \"::: split in multipage image of same size\" \
+     -split_colors[-1] 0 -rm[1] \
+     -e \"::: dilate and keep the rectanle around each object\" \
+     -dilate[1--1] 40 -repeat \{\$\!-1\}  --autocrop_seq[0,1] 0 -rm[1] -rm[-1]  -done -rm[0] -o $2
